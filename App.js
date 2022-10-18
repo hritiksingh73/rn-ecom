0ea3ec41e937327369880eac1182f8df688f05e9
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2022-10-18 14:18:18 +0530

    feat: rn-vector-icon on ios

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={styles.container} >
       <Text style={styles.heading} >Hello World</Text>
+      <Icon name="search1" size={30} color="#900" />
     </View>
   );
 }
@@ -12,10 +14,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "white"
   },
   heading: {
-    color: "white"
+
   }
 })
 
