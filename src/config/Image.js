f2273d056212259f9f5ede16d462974185f8ea54
Aditya khaparkar <aditya.k@indianic.com>
2022-10-27 16:00:30 +0530

    config-->Images.js File Added

---
+++ src/config/Image.js
@@ -0,0 +1,21 @@
+const imagePath = {
+  //Login Screen
+  grocerryMain: require('../asset/GrocerryMain.jpeg'),
+  orContinue: require('../asset/orcontinuewith.jpeg'),
+  facebook: require('../asset/facebook.jpeg'),
+  google: require('../asset/GoogleImage.jpeg'),
+
+  //Home Screen
+  artifacts: require('../asset/artifacts.webp'),
+  shoes: require('../asset/shoes.webp'),
+  clothing: require('../asset/clothing.webp'),
+  shoes: require('../asset/shoes.webp'),
+  flowering: require('../asset/flowering.webp'),
+  grocerry: require('../asset/grocerry.jpeg'),
+  laptop: require('../asset/laptop.jpeg'),
+
+  //Splash Screen
+  splash: require('../asset/SplashScreen.png'),
+};
+
+export default imagePath;
