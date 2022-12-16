6a6e30fbfb28e02d117a0c4850efdd4003e56b4b
Praveen Shakya IndiaNIC <praveen.shakya@indianic.com>
2022-12-16 09:40:12 +0530

    feat/added redux-persist

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import store from './src/redux/store/store';
+import store, {persistor} from './src/redux/store/store';
 import MainNavigator from './src/navigations/MainNavigator';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store} >
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer>
           <MainNavigator />
         </NavigationContainer>
-      {/* </PersistGate>  */}
+      </PersistGate> 
 	  </Provider>
   );
 };
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,32 +1,27 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userReducer from '../reducer/userReducer';
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+ 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistReducer(persistConfig, userReducer),
   },
-});
+})
 
 export default store;
-
+export const persistor = persistStore(store);
 
 // import {configureStore} from '@reduxjs/toolkit';
 // import userReducer from '../reducer/userReducer';
-// import { persistStore, persistReducer } from 'redux-persist'
-
-// import AsyncStorage from '@react-native-async-storage/async-storage';
- 
- 
-// const persistConfig = {
-//   key: 'root',
-//   storage: AsyncStorage,
-// }
-
 // const store = configureStore({
 //   reducer: {
-//     user: persistReducer(persistReducer, userReducer),
+//     user: userReducer,
 //   },
-// })
-
+// });
 // export default store;
-// export const persistor = persistStore(store);
